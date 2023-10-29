87ea39c77e4803702506ea3e9c845badfeff0bc8
tahaHelmy <124086650+tahaHelmy1121@users.noreply.github.com>
2023-10-29 19:36:39 +0200

    Update pinput.dart

--- lib/presntionlayer/auth/widget/pinput.dart
+++ lib/presntionlayer/auth/widget/pinput.dart
@@ -1,15 +1,1 @@
-//
-//
-// import 'package:flutter/cupertino.dart';
-//
-// import 'package:pinput/pinput.dart';
-// import 'package:provider/provider.dart';
-// import 'package:untitled1/busnieslogiclayer/auth/verifycodeviewmodel.dart';
-//
-// import '../../../busnieslogiclayer/auth/loginviewmodel.dart';
-//
-// Widget drewPinPut({var context }){
-//   return
-//
-//
-// }
+
